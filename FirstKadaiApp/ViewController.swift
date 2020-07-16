d9329a72d0190f251920c772c5b6edc4dee90b3d
y.tamu <qbs0006@icloud.com>
2020-07-16 10:31:18 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 0006 QBS on 2020/07/16.
+//  Copyright © 2020 0006 QBS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
